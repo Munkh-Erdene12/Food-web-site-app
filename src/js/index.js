b060602e8d4786f2675111729f4f8cdedfe43384
Munkh-Erdene12 <munkherdene.n1207@gmail.com>
2023-02-28 17:40:33 +0800

    index.js update

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,39 @@
 require("@babel/polyfill");
 import Search from "./model/search";
 
-let search = new Search("pasta");
+// let search = new Search("pasta");
 
-search.doSearch().then((r) => console.log(r));
+// search.doSearch().then((r) => console.log(r));
+
+/**
+ * web app төлөв
+ * тухайн үзүүлж байгаа жор
+ * лайкласан жорууд
+ * захиалж байгаа жорын найрлаганууд
+ */
+
+const state = {};
+
+const webApp = {
+  search: ".search",
+};
+
+const controlSearch = async () => {
+  // 1. вэбээс хайлтын түлхүүр үгийг гаргаж авна.
+  const query = "pizza";
+  if (query) {
+    // 2. шинээр хайлтын обьеютийг үүсгэж өгнө
+    state.search = new Search(query);
+    // 3. хайлт хийхэд зориулж дэлгэцийг UI бэлтгэнэ
+
+    // 4. хайлтыг гүйцэтгэнэ
+    await state.search.doSearch();
+    // 5. хайлтын үр дүнг дэлгэцэнд үзүүлнэ
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(webApp.search).addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
